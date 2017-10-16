fba545564ad56e0f713b9280dc97f5093895a40b
Teresa Kitwong <tuy_003@hotmail.com>
2017-10-16 09:25:52 -0500

    initial commit

---
+++ greeting.js
@@ -0,0 +1,7 @@
+var myName = prompt("What is you name?")
+
+function greeting(name) {
+   console.log("Hello! " + name);
+   }
+
+greeting(myName);
---
+++ height_to_centimeters.js
@@ -0,0 +1,2 @@
+var a = prompt("How tall are you in inches?")
+console.log(a + "inches" = "");
